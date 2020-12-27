3b157ce1398500b391072dcea6d9520da3d7082d
Yulevin <yulevinpost@gmail.com>
2020-12-27 22:56:23 +0300

    nothing important

--- mediaPlayer/scripts/musicPlayer.js
+++ mediaPlayer/scripts/musicPlayer.js
@@ -1,3 +1,31 @@
 export const musicPlayerInit = () => {
     console.log('"Music Player" инициализирован');
+
+    const audioPlayer = document.querySelector('.audio-player');
+    const audioBtnPrv = document.querySelector('.audio-button__prev');
+    const audioBtnPlay = document.querySelector('.audio-button__play');
+    const audioBtnNext = document.querySelector('.audio-button__next'); 
+    const audioTimePassed = document.querySelector('.audio-time__passed');
+    const audioProgressTime = document.querySelector('.audio-progress__timing');
+    const audioTimeTotal = document.querySelector('audio-time__total');
+
+    console.log(audioPlayer);
+    console.log(audioBtnPrv);
+    console.log(audioBtnPlay);
+    console.log(audioBtnNext);
+    console.log(audioTimePassed);
+    console.log(audioProgressTime);
+    console.log(audioTimeTotal);
+
+
+    const tooglePLay = () => {
+        if (audioPlayer.paused) {
+            audioPlayer.play();
+            console.log('[Play] - видео проигрывается');
+        } else {
+            audioPlayer.pause();
+            console.log('[Pause] - видео на паузе');
+        }
+    }
+    audioPlayer.addEventListener('click', tooglePLay);
 }
--- mediaPlayer/scripts/radioPlayer.js
+++ mediaPlayer/scripts/radioPlayer.js
@@ -1,3 +1,13 @@
 export const radioPlayerInit = () => {
     console.log('"Radio Player" инициализирован');
+
+    // Забираем классы кнопок управления
+    const radio = document.querySelector('.radio');
+    const radioCoverImg = document.querySelector('.radio-cover__img');
+    const radioHeader = document.querySelector('.radio-header');
+    const radioNavigation = document.querySelector('.radio-navigation');
+    const radioItem = document.querySelector('.radio-item');
+    const radioStop = document.querySelector('.radio-stop');
+    
+
 }
